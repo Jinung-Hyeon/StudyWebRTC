1ce08f88ee8fbc7f32c8bf45435a95c6159d25a3
JinUng Hyeon <ju.hyeon@smsoft.co.kr>
2022-08-25 17:26:03 +0900

    WebSocket 코드 주석, Socket.io 코드 시작

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,58 +1,60 @@
-const messageList = document.querySelector("ul");
-const nickForm = document.querySelector("#nick");
-const messageForm = document.querySelector("#message");
-const socket = new WebSocket(`ws://${window.location.host}`);
+// const messageList = document.querySelector("ul");
+// const nickForm = document.querySelector("#nick");
+// const messageForm = document.querySelector("#message");
+// const socket = new WebSocket(`ws://${window.location.host}`);
 
 
-function makeMessage(type, payload) {
-    const msg = {type, payload};
-    return JSON.stringify(msg);
-}
+// function makeMessage(type, payload) {
+//     const msg = {type, payload};
+//     return JSON.stringify(msg);
+// }
 
 
-// 소켓 서버에 연결됐을때
-socket.addEventListener("open", () => {
-    console.log("Connected to Server");
-});
+// // 소켓 서버에 연결됐을때
+// socket.addEventListener("open", () => {
+//     console.log("Connected to Server");
+// });
 
-// 서버에서 메시지를 받았을때
-socket.addEventListener("message", (message) => {
-    //console.log("New Message : ", message.data);
-    const li = document.createElement("li");
-    li.innerText = message.data;
-    messageList.append(li);
-});
+// // 서버에서 메시지를 받았을때
+// socket.addEventListener("message", (message) => {
+//     //console.log("New Message : ", message.data);
+//     const li = document.createElement("li");
+//     li.innerText = message.data;
+//     messageList.append(li);
+// });
 
-// 서버가 연결이 끊겼을때
-socket.addEventListener("close", () => {
-    console.log("Disconnected from Server");
-});
+// // 서버가 연결이 끊겼을때
+// socket.addEventListener("close", () => {
+//     console.log("Disconnected from Server");
+// });
 
 
-// setTimeout(() => {
-//     socket.send("hello from the browser!");
-// }, 10000);
+// // setTimeout(() => {
+// //     socket.send("hello from the browser!");
+// // }, 10000);
 
-function handleSubmit(event) {
-    event.preventDefault();
-    const input = messageForm.querySelector("input");
-    socket.send(makeMessage("new_message", input.value));
-    const li = document.createElement("li");
-    li.innerText = `You: ${input.value}`;
-    messageList.append(li);
-    //console.log(input.value);
-    input.value = "";
+// function handleSubmit(event) {
+//     event.preventDefault();
+//     const input = messageForm.querySelector("input");
+//     socket.send(makeMessage("new_message", input.value));
+//     const li = document.createElement("li");
+//     li.innerText = `You: ${input.value}`;
+//     messageList.append(li);
+//     //console.log(input.value);
+//     input.value = "";
     
-}
+// }
 
 
 
-function handleNickSubmit(event) {
-    event.preventDefault();
-    const input = nickForm.querySelector("input");
-    socket.send(makeMessage("nickname", input.value));
-    input.value = "";
-}
+// function handleNickSubmit(event) {
+//     event.preventDefault();
+//     const input = nickForm.querySelector("input");
+//     socket.send(makeMessage("nickname", input.value));
+//     input.value = "";
+// }
 
-messageForm.addEventListener("submit", handleSubmit);
-nickForm.addEventListener("submit", handleNickSubmit);
+// messageForm.addEventListener("submit", handleSubmit);
+// nickForm.addEventListener("submit", handleNickSubmit);
+
+const socket = io();
--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import http from "http";
-import WebSocket from "ws";
+import SocketIo from "socket.io";
+//import WebSocket from "ws";
 import express from "express";
 import e from "express";
+import { Socket } from "dgram";
 
 const app = express();
 
@@ -15,8 +17,13 @@ app.get("/*", (_, res) => res.redirect("/"));
 const handleListen = () => console.log('Listening on http://localhost:3000');
 // app.listen(3000, handleListen);
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const httpServer = http.createServer(app);
+// const wss = new WebSocket.Server({ server });
+const wsServer = SocketIo(httpServer);
+
+wsServer.on("connection", socket => {
+    console.log(socket);
+});
 
 // function handleConnection(socket){
 //     console.log(socket);
@@ -24,28 +31,28 @@ const wss = new WebSocket.Server({ server });
 
 // wss.on("connection", handleConnection);
 
-const sockets = []; // 연결되는 브라우저들을 저장하는 배열 생성
+// const sockets = []; // 연결되는 브라우저들을 저장하는 배열 생성
 
-wss.on("connection", (socket) => { // 브라우저와 연결이되면
-    //console.log(socket);
-    sockets.push(socket);
-    socket["nickname"] = "Anon";
-    console.log("Connected to Browser");
-    socket.on("close", () => console.log("Disconnected from the Browser")); // 브라우저 연결이 끊겼을때
-    socket.on("message", (msg) => { // 브라우저에서 메시지를 보낼때
-        const message = JSON.parse(msg);
-        //console.log(parsed, message.toString());
+// wss.on("connection", (socket) => { // 브라우저와 연결이되면
+//     //console.log(socket);
+//     sockets.push(socket);
+//     socket["nickname"] = "Anon";
+//     console.log("Connected to Browser");
+//     socket.on("close", () => console.log("Disconnected from the Browser")); // 브라우저 연결이 끊겼을때
+//     socket.on("message", (msg) => { // 브라우저에서 메시지를 보낼때
+//         const message = JSON.parse(msg);
+//         //console.log(parsed, message.toString());
 
-        switch(message.type){
-            case "new_message":
-                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
-            case "nickname":
-                socket["nickname"] = message.payload;
+//         switch(message.type){
+//             case "new_message":
+//                 sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+//             case "nickname":
+//                 socket["nickname"] = message.payload;
 
-        }
-    });
-    //socket.send("hello!!"); // 브라우저에 메시지 보냄
-});
+//         }
+//     });
+//     //socket.send("hello!!"); // 브라우저에 메시지 보냄
+// });
 
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
 
