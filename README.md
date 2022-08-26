f727db5849c059d900301ae4edf84b07dd49719b
Jinung Hyeon <oiuoiyu@naver.com>
2022-08-26 21:08:11 +0900

    done chatTest

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Noom
 
 Zoom Clone using NodeJS, WebRTC and Websockets.
+
+npm i nodemon
+npm i @babel/core @babel/cli @babel/node -D
+npm i @babel/preset-env -D
+npm i ws
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import http from "http";
-import SocketIo from "socket.io";
+import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui"
 //import WebSocket from "ws";
 import express from "express";
 import e from "express";
@@ -19,10 +20,68 @@ const handleListen = () => console.log('Listening on http://localhost:3000');
 
 const httpServer = http.createServer(app);
 // const wss = new WebSocket.Server({ server });
-const wsServer = SocketIo(httpServer);
+const wsServer = new Server(httpServer, {
+    cors: {
+        origin: ["https://admin.socket.io"],
+        credentials: true,
+    },
+});
 
-wsServer.on("connection", socket => {
-    console.log(socket);
+instrument(wsServer, {
+    auth: false
+});
+
+
+function publicRooms() {
+
+    const {
+        sockets: {
+            adapter: { sids, rooms },
+        },
+    } = wsServer;
+
+    const publicRooms = [];
+    rooms.forEach((_, key) => {
+        if (sids.get(key) === undefined) {
+            publicRooms.push(key);
+        }
+    });
+
+    return publicRooms;
+}
+
+function countRoom(roomName) {
+    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+
+
+wsServer.on("connection", (socket) => {
+    socket["nickname"] = "Anon";
+    socket.onAny((event) => {
+        console.log(`Socket Event:${event}`);
+    });
+    //console.log(socket);
+    socket.on("enter_room", (roomName, done) => {
+        socket.join(roomName);
+        done();
+        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
+        wsServer.sockets.emit("room_change", publicRooms());
+    });
+
+    socket.on("disconnecting", () => {
+        socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1));
+    });
+
+    socket.on("disconnect", () => {
+        wsServer.sockets.emit("room_change", publicRooms());
+    });
+
+    socket.on("new_message", (msg, room, done) => {
+        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
+        done();
+    });
+
+    socket.on("nickname", (nickname) => socket["nickname"] = nickname);
 });
 
 // function handleConnection(socket){
